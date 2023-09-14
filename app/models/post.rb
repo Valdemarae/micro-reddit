c309d88a98c6309cb0e4e3e047f8aca9df6a39c1
Valdemar <valdemar.kobelis@gmail.com>
2023-09-14 18:07:32 +0300

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,3 @@
 class Post < ApplicationRecord
-  validates :url, presence: true
+  validates :url, presence: true, format: { with: /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix }
 end
